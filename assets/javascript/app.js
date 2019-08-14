b0ad7c7997dc0dd1ad232a7391df54144e4bd779
Brian Yeager <rbrianyeager@gmail.com>
2019-08-13 21:09:49 -0400

    Psuedocode and skeleton javascript logic

---
+++ assets/javascript/app.js
@@ -0,0 +1,69 @@
+
+
+var right = 0
+var wrong = 0
+var unanswered = 0
+
+// User starts game
+
+// Countdown timer begins
+
+// Question 1
+
+// Question 1 conditionals
+var choices1 = ["Porsche 959", "Ferrari F40", "Lamborghini Countach", "BMW M1"]
+// var userGuess1 button click something
+if (userGuess === choices1[1]) {
+    console.log("Correct!");
+} else {
+    console.log("Wrong!");
+}
+
+// Question 2
+
+// Question 2 conditionals
+var choices2 = ["Mercedes Benz", "Lamborghini", "Ferrari", "Pagani"]
+// var userGuess2 button click something
+if (userGuess === choices2[1]) {
+    console.log("Correct!");
+} else {
+    console.log("Wrong!");
+}
+
+// Question 3
+
+// Question 3 conditionals
+var choices3 = ["4", "2", "1", "3"]
+// var userGuess3 button click something
+
+if (userGuess === choices3[3]) {
+    console.log("Correct!");
+} else {
+    console.log("Wrong!");
+}
+
+// Question 4
+
+// Question 4 conditionals
+var choices4 = ["The Bug", "The Widowmaker", "The German", "Whale Tail"]
+// var userGuess4 button click something
+
+if (userGuess === choices4[1]) {
+    console.log("Correct!");
+} else {
+    console.log("Wrong!");
+}
+
+// Question 5
+
+// Question 5 conditionals
+var choices5 = ["Bentley", "BMW", "Aston Martin", "Rolls Royce"]
+// var userGuess5 button click something
+
+if (userGuess === choices5[0]) {
+    console.log("Correct!");
+} else {
+    console.log("Wrong!");
+}
+
+//User submits answers
